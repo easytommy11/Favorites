c4189e68ef1b0bd83bf82fbd903a3bd5b597ad1f
Tomiwa Babalola <tbabalola004@gmail.com>
2021-11-01 12:17:32 -0400

    vjhvkhgv

--- Favorites/ViewController.swift
+++ Favorites/ViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var images: [UIImage] = [#imageLiteral(resourceName: "Power1"), #imageLiteral(resourceName: "You"), #imageLiteral(resourceName: "power"), #imageLiteral(resourceName: "naruto"), #imageLiteral(resourceName: "allAmerican")]
+    var heading: [String] = ["Power", "You", "Power:Book II", "Naruto", "All American"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
 
