2a2818dcb01ec79b34c4b6ec308b5f5428e3de6f
Tomiwa Babalola <tbabalola004@gmail.com>
2021-11-02 09:27:04 -0400

    jdjsbk

--- Favorites/ViewController.swift
+++ Favorites/ViewController.swift
@@ -8,14 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var Label: UILabel!
+    
     var images: [UIImage] = [#imageLiteral(resourceName: "Power1"), #imageLiteral(resourceName: "You"), #imageLiteral(resourceName: "power"), #imageLiteral(resourceName: "naruto"), #imageLiteral(resourceName: "allAmerican")]
     var heading: [String] = ["Power", "You", "Power:Book II", "Naruto", "All American"]
-    
 
+    var position = 0
+    
+   
+    
+    @IBAction func Button(_ sender: Any) {
+        if(position < images.count){
+            Label.text = heading[position]
+            imageView.image = images[position]
+            position = position + 1
+        }
+        else{
+            position = 0
+        }
+      
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+    
         // Do any additional setup after loading the view.
     }
 
